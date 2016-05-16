1819a7ea944675e53956f052f38889b9554e0dd1
Admin <manivan.iv@gmail.com>
2016-05-17 01:06:19 +0300

    qwe

--- appcoment/models.py
+++ appcoment/models.py
@@ -6,12 +6,12 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+#http://proft.me/2010/09/7/drevovidnye-struktury-dannyh-v-django/
 @python_2_unicode_compatible
 class PostModel(models.Model):
     title = models.CharField(max_length=200)
     conten = models.TextField()
     timecreated = models.DateTimeField(verbose_name='Data Created', auto_now_add=True)
-
 
     def __str__(self):
         return self.title
@@ -20,15 +20,23 @@ class PostModel(models.Model):
 class CommentAbs(models.Model):
     textcomment = models.CharField(max_length=100)
     likecom = models.CharField(max_length=1, default='+')
-    #content_type = models.ForeignKey(ContentType, object_pk = models.TextField(_('object ID')))
+    parent = models.ForeignKey('self', blank=True, null=True, verbose_name="Parent", related_name='child')
+
+    def __str__(self):
+        return self.textcomment
 
     class Meta:
         abstract = True
 
 
+
 @python_2_unicode_compatible
 class CommentList(CommentAbs):
     autor = models.ForeignKey(User)
+    comtopost = models.ForeignKey(PostModel, default=1)
+
+    def get_absolute_url(self):
+        return "/post/%i/" % self.pk
 
     def __str__(self):
         return self.textcomment
@@ -37,9 +45,10 @@ class CommentList(CommentAbs):
 class Com(models.Model):
     comtopost = models.ForeignKey(CommentList)
 
+    """    def __str__(self):
+        return self.pk
 
 
-"""
 class FooModel(models.Model):
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children')
     children = models.ManyToOneRel('self', blank=True, null=True)
--- appcoment/views.py
+++ appcoment/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render_to_response
-from .models import CommentAbs, CommentList, Com
+from .models import CommentAbs, CommentList, Com, PostModel
 
 
 def newcabs(request):
     autor = CommentList.objects.all()
-    post = Com.objects.all()
-
-    return render_to_response('base.html', {'autor': autor, 'post': post})
+    comments = Com.objects.all()
+    post = PostModel.objects.all()
+    return render_to_response('base.html', {'autor': autor, 'post': post, 'comments': comments})
