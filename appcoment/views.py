d3a5dc54d3325cfc14edae812894636b6475e61f
Admin <manivan.iv@gmail.com>
2016-05-17 22:24:03 +0300

    1

--- appcoment/views.py
+++ appcoment/views.py
@@ -3,7 +3,7 @@ from .models import CommentAbs, CommentList, Com, PostModel
 
 
 def newcabs(request):
-    autor = CommentList.objects.all()
+    comls = CommentList.objects.all()
     comments = Com.objects.all()
     post = PostModel.objects.all()
-    return render_to_response('base.html', {'autor': autor, 'post': post, 'comments': comments})
+    return render_to_response('base.html', {'comls': comls, 'post': post, 'comments': comments})
