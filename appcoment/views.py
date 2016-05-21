bbd183f60d7887175e9dc4655f5ef33dfde24f0b
Admin <manivan.iv@gmail.com>
2016-05-21 23:58:08 +0300

    del

--- appcoment/views.py
+++ appcoment/views.py
@@ -5,16 +5,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.template import RequestContext
 
 
-def show_genres(request):
-    return render_to_response("genres.html",
-                          { 'nodes': CommentList.objects.all()},
-                          context_instance=RequestContext(request))
-
-
 
 def postcoments(request, pk):
     post = PostModel.objects.filter(pk=pk)
-    #allcom = CommentList.objects.all()
     contact_list = CommentList.objects.select_related('textcom').filter(textcom=pk)
     paginator = Paginator(contact_list, 5) # Show 5 contacts per page
 
@@ -29,8 +22,8 @@ def postcoments(request, pk):
         # If page is out of range (e.g. 9999), deliver last page of results.
         contacts = paginator.page(paginator.num_pages)
 
-    return render_to_response('postcoments.html', {"contacts": contacts, 'post': post, 'nodes': contacts}
-                              , context_instance=RequestContext(request))
+    return render_to_response('postcoments.html', {"contacts": contacts, 'post': post, 'nodes': contacts})
+                              #, context_instance=RequestContext(request))
 
 def post_update(request, pk):
     instance = get_object_or_404(CommentList, id=pk)
@@ -68,36 +61,9 @@ def newcabspk(request, pk):
     allcom = CommentList.objects.all()
     return render_to_response('postcom.html', {'comls': comls, 'post': post, 'allcom': allcom})
 
-#https://www.sitepoint.com/hierarchical-data-database/
-#http://py-algorithm.blogspot.com/2011/07/blog-post_30.html
-#http://proft.me/2010/09/7/drevovidnye-struktury-dannyh-v-django/
 
-def listing(request):
-    contact_list = CommentList.objects.all()
-    paginator = Paginator(contact_list, 5) # Show 5 contacts per page
-
-    page = request.GET.get('page')
-    try:
-        contacts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        contacts = paginator.page(1)
-    except EmptyPage:
-        #http://djbook.ru/rel1.8/topics/pagination.html#using-paginator-in-a-view
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        contacts = paginator.page(paginator.num_pages)
-
-    return render_to_response('list.html', {"contacts": contacts})
 
 def newcabs(request):
     comls = CommentList.objects.all()
     post = PostModel.objects.all()
     return render_to_response('base.html', {'comls': comls, 'post': post})
-
-
-class ComentListView(ListView):
-    model = CommentList
-
-    def head(self,*args, **kwargs):
-        post = PostModel.objects.all()
-        return render_to_response('base.html', {'post': post})
--- testcomment/urls.py
+++ testcomment/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from appcoment.views import newcabs, ComentListView, newcabspk, listing, lpost, show_genres, postcoments, post_update
+from appcoment.views import newcabs, newcabspk, lpost, postcoments, post_update
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -23,9 +23,6 @@ urlpatterns = [
     url(r'^newcabspk/(?P<pk>[0-9]+)/$', newcabspk),
     url(r'^postcoments/(?P<pk>[0-9]+)/$', postcoments),
     url(r'^lpost/(?P<pk>[0-9]+)/$', lpost),
-    url(r'^coments/$', ComentListView.as_view()),
-    url(r'^listing/$', listing),
-    url(r'^genres/$', show_genres),
     url(r'^(?P<pk>[0-9]+)/update/$', post_update),
 
 ]
